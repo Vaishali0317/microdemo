43d9ab238019fd18ff615636872cb05f77b113bf
vaish0317 <vaishalikatariya480@gmail.com>
2019-05-28 13:02:51 -0400

    index.ts added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { ProductListComponent } from "./product-list/product-list.component";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { CartComponent } from './cart/cart.component';
-import { HomeComponent } from './home/home.component';
-import { ProductListModule } from './product-list/product-list.module';
-import { ProvisionComponent } from './provision/provision.component';
-import { TopbarComponent } from './topbar/topbar.component';
-import { AppRoutingModule } from './app-routing.module';
-import { BillingModule } from './billing/billing.modules';
+import { AppComponent } from "./app.component";
+import { CartComponent } from "./cart/cart.component";
+import { HomeComponent } from "./home/home.component";
+import { ProductListModule } from "./product-list/product-list.module";
+import { ProvisionComponent } from "./provision/provision.component";
+import { TopbarComponent } from "./topbar/topbar.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { BillingModule } from "./billing/billing.modules";
 
 @NgModule({
   declarations: [
@@ -17,14 +18,10 @@ import { BillingModule } from './billing/billing.modules';
     HomeComponent,
     ProvisionComponent,
     TopbarComponent
-    ],
-  imports: [
-    BrowserModule,
-    ProductListModule,
-    BillingModule,
-    AppRoutingModule
   ],
+  imports: [BrowserModule, ProductListModule, BillingModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [ProductListComponent]
 })
-export class AppModule { }
+export class AppModule {}
